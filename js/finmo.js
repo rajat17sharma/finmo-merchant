30a22332906e0031bd45ff3e576b43c1201ecb51
Rajat <rajatdevsharma03@gmail.com>
2017-06-01 10:56:19 +0530

    code till register form

--- js/finmo.js
+++ js/finmo.js
@@ -55,6 +55,19 @@ $(document).ready(function() {
         $(".nav-toggle").toggleClass("active");
         $(".overlay-finmo").toggleClass("open");
     });
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 2000);
+            return false;
+        }
+    }
+});
 
 
 });
---
+++ js/side_nav.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "35%";
+    document.getElementById("main").style.marginLeft = "35%";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.body.style.background.opacity="0.5"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    // document.backgroundColorody.style.backgroundColor = "white";
+}
